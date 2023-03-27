d9dceb909222feb433084c2f79fcad2ad9f71ca6
Нікіта Скиба <128480412+nikitosachka@users.noreply.github.com>
2023-03-28 00:21:33 +0300

    Add files via upload

--- Lab1/Cars.py
+++ Lab1/Cars.py
@@ -10,3 +10,10 @@ height = 500
 screen_size = (width, height)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Need For Speed')
+
+#кольора
+gray = (100, 100, 100)
+green = (76, 208, 56)
+red = (200, 0, 0)
+white = (255, 255, 255)
+yellow = (255, 232, 0)
