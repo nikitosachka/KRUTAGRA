62cdaad25c4c0b4018c4a6002d32243acd6e443d
Нікіта Скиба <128480412+nikitosachka@users.noreply.github.com>
2023-04-25 02:53:21 +0300

    Update test_Cars.py

--- test_Cars.py
+++ test_Cars.py
@@ -18,3 +18,12 @@ def test_player_move_left(player):
 def test_player_move_right(player):
     player.rect.x += 100
     assert player.rect.center == (350, 400)
+
+def test_score():
+    score = 0
+    score += 2
+    assert score == 2
+    score += 3
+    assert score == 5
+    score += 5
+    assert score == 10
